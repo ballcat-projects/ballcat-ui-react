cfcc47a3aa53548d39a9f7239b68073c6e93f7df
聆听 <sunlisten.gzm@gmail.com>
2021-07-04 16:35:08 +0800

    feat: 添加请求全局处理

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import { PageLoading } from '@ant-design/pro-layout';
 import { notification } from 'antd';
 import type { RequestConfig, RunTimeLayoutConfig } from 'umi';
 import { history, Link } from 'umi';
+import type { RequestInterceptor, RequestOptionsInit } from 'umi-request';
 import RightContent from '@/components/RightContent';
 import Footer from '@/components/Footer';
 import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
@@ -26,8 +27,7 @@ export async function getInitialState(): Promise<{
 }> {
   const fetchUserInfo = async () => {
     try {
-      const currentUser = await queryCurrentUser();
-      return currentUser;
+      return await queryCurrentUser();
     } catch (error) {
       history.push(loginPath);
     }
@@ -49,51 +49,65 @@ export async function getInitialState(): Promise<{
 }
 
 /**
- * 异常处理程序
-    200: '服务器成功返回请求的数据。',
-    201: '新建或修改数据成功。',
-    202: '一个请求已经进入后台排队（异步任务）。',
-    204: '删除数据成功。',
-    400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-    401: '用户没有权限（令牌、用户名、密码错误）。',
-    403: '用户得到授权，但是访问是被禁止的。',
-    404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-    405: '请求方法不被允许。',
-    406: '请求的格式不可得。',
-    410: '请求的资源被永久删除，且不会再得到的。',
-    422: '当创建一个对象时，发生一个验证错误。',
-    500: '服务器发生错误，请检查服务器。',
-    502: '网关错误。',
-    503: '服务不可用，服务器暂时过载或维护。',
-    504: '网关超时。',
- //-----English
-    200: The server successfully returned the requested data. ',
-    201: New or modified data is successful. ',
-    202: A request has entered the background queue (asynchronous task). ',
-    204: Data deleted successfully. ',
-    400: 'There was an error in the request sent, and the server did not create or modify data. ',
-    401: The user does not have permission (token, username, password error). ',
-    403: The user is authorized, but access is forbidden. ',
-    404: The request sent was for a record that did not exist. ',
-    405: The request method is not allowed. ',
-    406: The requested format is not available. ',
-    410':
-        'The requested resource is permanently deleted and will no longer be available. ',
-    422: When creating an object, a validation error occurred. ',
-    500: An error occurred on the server, please check the server. ',
-    502: Gateway error. ',
-    503: The service is unavailable. ',
-    504: The gateway timed out. ',
+ * 自定义拦截请求
+ */
+const customerRequestInterceptor: RequestInterceptor = (
+  url: string,
+  options: RequestOptionsInit,
+): {
+  url?: string;
+  options?: RequestOptionsInit;
+} => {
+  // 处理请求地址
+  const newUrl = `/api/${url.startsWith('/') ? url.substring(1) : url}`;
+  const headers = { Authorization: '', ...options.headers };
+
+  // 添加token
+  if (!headers.Authorization) {
+    headers.Authorization = `Bearer ${localStorage.getItem('access-key')}`;
+  }
+
+  return { url: newUrl, options: { ...options, headers } };
+};
+
+/**
+ * 请求增强
  * @see https://beta-pro.ant.design/docs/request-cn
  */
 export const request: RequestConfig = {
+  requestType: 'json',
+  requestInterceptors: [customerRequestInterceptor],
+  responseInterceptors: [
+    (res) => {
+      return res
+        .clone()
+        .json()
+        .then((json) => {
+          if (json && json.code !== 200) {
+            // eslint-disable-next-line @typescript-eslint/no-throw-literal
+            throw {
+              response: res,
+              data: json,
+              message: json.message,
+            };
+          }
+
+          return res;
+        });
+    },
+  ],
   errorHandler: (error: any) => {
-    const { response } = error;
+    const { response, message } = error;
 
     if (!response) {
       notification.error({
         description: '您的网络发生异常，无法连接服务器',
         message: '网络异常',
+      });
+    } else {
+      notification.error({
+        description: message,
+        message: '请求异常',
       });
     }
     throw error;
