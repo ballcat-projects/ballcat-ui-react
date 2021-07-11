4d11179e85497e6775976d5cecd6f63e17c9db64
聆听 <sunlisten.gzm@gmail.com>
2021-07-11 18:13:35 +0800

    fix: 修复确实的404

--- src/pages/exception/404.tsx
+++ src/pages/exception/404.tsx
@@ -6,10 +6,15 @@ const NoFoundPage: React.FC = () => (
   <Result
     status="404"
     title="404"
-    subTitle="Sorry, the page you visited does not exist."
+    subTitle="抱歉，您访问的页面不存在!"
     extra={
-      <Button type="primary" onClick={() => history.push('/')}>
-        Back Home
+      <Button
+        type="primary"
+        onClick={() => {
+          history.push('/');
+        }}
+      >
+        返回首页
       </Button>
     }
   />
