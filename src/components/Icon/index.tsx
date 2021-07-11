350efb29c5076c893106025c83aa461637c67139
聆听 <sunlisten.gzm@gmail.com>
2021-07-11 19:14:17 +0800

    fix: 修复Icon代码提示

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,6 +1,15 @@
 import { createFromIconfontCN } from '@ant-design/icons';
+import type React from 'react';
 import settings from '../../../config/defaultSettings';
 
-export const Icon = createFromIconfontCN({
+interface IconProps {
+  style?: React.CSSProperties;
+  onClick?: (e: any) => void;
+  type: string;
+
+  [key: string]: any;
+}
+
+export const Icon: React.FC<IconProps> = createFromIconfontCN({
   scriptUrl: settings.iconfontUrl,
 });
