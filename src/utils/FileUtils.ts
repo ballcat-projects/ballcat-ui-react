606a6e451f83b7c71a3830fc87e149ef445e4a2c
lingting <sunlisten.gzm@gmail.com>
2021-09-19 12:27:57 +0800

    feat(Util): 添加文件下载方法

---
+++ src/utils/FileUtils.ts
@@ -0,0 +1,19 @@
+async function remoteFileDownload(blob: Blob, fileName?: string) {
+  const name = fileName || new Date().getTime().toString();
+  // @ts-ignore
+  if (window.navigator.msSaveOrOpenBlob) {
+    // 兼容IE10
+    // @ts-ignore
+    navigator.msSaveBlob(blob, fileName);
+  } else {
+    const href = URL.createObjectURL(blob); // 创建新的URL表示指定的blob对象
+    const a = document.createElement('a'); // 创建a标签
+    a.style.display = 'none';
+    a.href = href; // 指定下载链接
+    a.download = name; // 指定下载文件名
+    a.click(); // 触发下载
+    URL.revokeObjectURL(a.href); // 释放URL对象
+  }
+}
+
+export default { remoteFileDownload };
