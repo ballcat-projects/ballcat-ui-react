e6d50c16f59872f9cbd930e262d9ed12be0787f1
lingting <sunlisten.gzm@gmail.com>
2021-10-19 14:53:51 +0800

    feat(Login): 登录状态过期不直接跳转登录页,而是弹出提示框, 引导重新登陆

--- src/utils/NotifyUtils.tsx
+++ src/utils/NotifyUtils.tsx
@@ -1,6 +1,7 @@
 import { Modal } from 'antd';
 import { announcement } from '@/services/ballcat/notify';
 import { NotificationOutlined } from '@ant-design/icons';
+import RouteUtils from './RouteUtils';
 
 export type NotifyProps = { id: string; content: string; title: string };
 
@@ -29,6 +30,17 @@ const Notify = {
       closable: false,
       onOk: () => readNotice(id),
       onCancel: () => readNotice(id),
+    });
+  },
+  logout: () => {
+    Modal.info({
+      title: '系统提示',
+      content: '登录状态已过期, 请退出重新登录!',
+      closable: false,
+      keyboard: false,
+      okText: '重新登录',
+      onOk: () => RouteUtils.redirect('/user/login'),
+      onCancel: () => RouteUtils.redirect('/user/login'),
     });
   },
 };
