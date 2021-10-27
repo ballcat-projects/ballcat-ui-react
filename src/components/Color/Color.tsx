f0f7f8f367042b93c46523705aa3b65758a72ad0
lingting <sunlisten.gzm@gmail.com>
2021-10-27 15:18:17 +0800

    feat(Color): 添加默认颜色

--- src/components/Color/Color.tsx
+++ src/components/Color/Color.tsx
@@ -7,16 +7,17 @@ import { SvgIcon } from '@/components/Icon';
 import { DeleteOutlined } from '@ant-design/icons';
 
 export const DEFAULT_COLORS = [
-  '#FF9D4E', // 0 - 橘黄色
-  '#5BD8A6', // 1 - 绿色
-  '#5B8FF9', // 2 - 蓝色
-  '#F7664E', // 3 - 红色
-  '#FF86B7', // 4 - 水红色
-  '#2B9E9D', // 5 - 墨绿色
-  '#9270CA', // 6 - 紫色
-  '#6DC8EC', // 7 - 浅蓝色
-  '#667796', // 8 - 黛蓝色
-  '#F6BD16', // 9 - 黄色
+  '#FF9D4E', // 橘黄色
+  '#5BD8A6', // 绿色
+  '#5B8FF9', // 蓝色
+  '#F7664E', // 红色
+  '#FF86B7', // 水红色
+  '#2B9E9D', // 墨绿色
+  '#9270CA', // 紫色
+  '#6DC8EC', // 浅蓝色
+  '#667796', // 黛蓝色
+  '#F6BD16', // 黄色
+  '#d9d9d9', // 灰色
 ];
 
 const Color = (props: ColorProps) => {
