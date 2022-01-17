0c82c6757c0bdd00b416083193249f0b4b0f5fac
lingting <sunlisten.gzm@gmail.com>
2022-01-17 12:40:40 +0800

    fix: 移除无用配置

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -4,9 +4,6 @@ export default defineConfig({
   define: {
     'process.env': {
       msg: '现在是 prod 环境!',
-      // 如果要使用. 需要自己申请 google cloud api
-      // https://console.cloud.google.com/google/maps-apis/overview
-      google_map_api: 'AIzaSyDpi5kQbJ19Pb3Y6u75ALDSTejWJEVgGVE',
     },
   },
 });
